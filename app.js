5538bf2d71dbc88553a2df25851721b8e7d10522
lucianosls <luchosalas111@gmail.com>
2022-06-14 22:15:07 -0300

    carpetas views ordenadas

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/productdetail", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+    res.sendFile(path.join(__dirname, "/views/products/productDetail.html"));
 })
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html"));
+    res.sendFile(path.join(__dirname, "/views/users/login.html"));
 })
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html"));
+    res.sendFile(path.join(__dirname, "/views/users/register.html"));
 })
 
 app.get("/shoppingcart", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/carritoCompras.html"));
+    res.sendFile(path.join(__dirname, "/views/products/carritoCompras.html"));
 })
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/products/home.html"));
 })
 
 app.listen(process.env.PORT || 3000, () => {
