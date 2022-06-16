8989b87e80fe7fa3f5355a52213f3224bc905bb6
JuanSAuzaO <sebastianauza@hotmail.com>
2022-06-16 12:13:19 -0500

    Vistas parciales incorporadas

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.set('view engine', 'ejs');
 app.set('views', './views')
