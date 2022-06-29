d3e2f78fa29f7498a4fdb33e010f0cfca9f5cb34
JuanSAuzaO <sebastianauza@hotmail.com>
2022-06-29 12:27:43 -0500

    put updated

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/products/:id/edit', controller.edit);
+router.put('/products/:id', controller.update);
 
 module.exports = router;
 
